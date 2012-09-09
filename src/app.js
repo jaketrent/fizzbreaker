f82a548dc1327076b0171bcdbe8fd3a92c0a81f1
jtsnake <trent.jake@gmail.com>
2012-09-09 15:47:09 -0600

    set favicon

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.configure(function(){
   app.set('port', process.env.PORT || 5000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon());
+  app.use(express.favicon(__dirname + '/static/img/favicon.ico'));
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
